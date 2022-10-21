76309564032ce4242c2cc655bb8eec653c9b4360
Vhuhwavho Ramutsindela <vhuhwavhoramutsindela@gmail.com>
2022-10-21 11:06:57 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,16 +8,15 @@ void more_numbers(void)
 {
 	int num, a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (num = 0; num <= 14; num++)
-
+		for (num = 0; num < 15; num++)
+		{
 			if (num > 9)
-			{
-				_putchar((num / 10) + '0');
-			}
-		_putchar((num % 10) + '0');
-	}
+			_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
 
-	_putchar('\n');
+			}
+		_putchar('\n');
+	}
 }
